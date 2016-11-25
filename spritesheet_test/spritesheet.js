8613b066920413839aafccfbf90cc32a745762f1
yoonaog01 <snsd.yoonaog@gmail.com>
2016-11-26 02:36:29 +0800

    js all motions

--- spritesheet_test/spritesheet.js
+++ spritesheet_test/spritesheet.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create });
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, stand: stand, walk: walk, jump: jump, climb: climb, attack: attack });
 
 var sprite;
 
@@ -8,20 +8,47 @@ function preload() {
     //  There are 18 frames in the PNG - you can leave this value blank if the frames fill up the entire PNG, but in this case there are some
     //  blank frames at the end, so we tell the loader how many to load
                 
-    game.load.spritesheet('chr', 'sample.png', 91, 95, 11);
+    game.load.spritesheet('chr', 'sample.png', 92, 95, 11);
 
   }
 
-function create() {
+function stand() {
 
     sprite = game.add.sprite(40, 100, 'chr');
-    sprite.animations.add('walk');
-    sprite.animations.play('walk',2,true);
-//    game.add.tween(sprite).to({ x: game.width }, 10000, Phaser.Easing.Linear.None, true);
+    sprite.animations.add('stand',[0]);
+    sprite.animations.play('stand',2,true);
 
   }
 
-    //  update isn't called until 'create' has completed. If you need to process stuff before that point (i.e. while the preload is still happening)
-    //  then create a function called loadUpdate() and use that
-    
+function walk() {
+
+    sprite = game.add.sprite(140, 100, 'chr');
+        sprite.animations.add('walk',[1, 2, 3, 4]);
+            sprite.animations.play('walk',2,true);
+
+              }
+
+function jump() {
+
+    sprite = game.add.sprite(240, 100, 'chr');
+        sprite.animations.add('jump',[5]);
+            sprite.animations.play('jump',2,true);
+
+              }
+
+function climb() {
+
+    sprite = game.add.sprite(340, 100, 'chr');
+        sprite.animations.add('climb',[6, 7]);
+            sprite.animations.play('climb',2,true);
+
+              }
+
+function attack() {
+
+    sprite = game.add.sprite(440, 100, 'chr');
+        sprite.animations.add('attack',[8, 9, 10]);
+            sprite.animations.play('attack',2,true);
+
+              }
 
