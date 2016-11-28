3be0f515730b4919b22aface062a5d29a613782e
yoonaog01 <snsd.yoonaog@gmail.com>
2016-11-28 14:41:26 +0800

    js

--- spritesheet_test/spritesheet.js
+++ spritesheet_test/spritesheet.js
@@ -34,25 +34,14 @@ function update() {
         sprite.animations.play('walk',5,true);
     }
 
-    if (game.input.keyboard.justReleased(Phaser.Keyboard.LEFT , 50)){
-        sprite.body.velocity.x = 0;
-        sprite.animations.play('stand',5,true);
-    }
-
-/*    else {
-        sprite.body.velocity.x = 0;
-        sprite.animations.play('stand',5,true);
-    }*/
-
     if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
         sprite.body.velocity.x = 120;
         sprite.animations.play('walk',5,true);
     }
 
- /*   else {
+    if(!game.input.keyboard.isDown(Phaser.Keyboard.RIGHT) && !game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
         sprite.body.velocity.x = 0;
-        sprite.animations.play('stand',5,true);
-    }*/
+    }
 
     if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
         sprite.body.velocity.x = 0;
@@ -66,7 +55,7 @@ function update() {
         sprite.animations.play('climb',5,true);
     }
 
-    if (game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
+    if (game.input.keyboard.isDown(Phaser.Keyboard.ALT)) {
          sprite.body.velocity.y = -350;
          sprite.animations.play('jump',5,true);
     }
@@ -74,5 +63,7 @@ function update() {
     if (game.input.keyboard.isDown(Phaser.Keyboard.CONTROL)) {
         sprite.animations.play('attack',5,true);
     }
+
+
 
 }
