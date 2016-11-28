8123d43b6f825ea063eb16ec74930934e2f1580d
yoonaog01 <snsd.yoonaog@gmail.com>
2016-11-28 13:43:45 +0800

    js

--- spritesheet_test/spritesheet.js
+++ spritesheet_test/spritesheet.js
@@ -49,7 +49,7 @@ function update() {
         sprite.animations.play('climb',5,true);
     }
 
-    else if (game.input.keyboard.isDOWN(Phaser.Keyboard.X) && sprite.body.touching.down) {
+    else if (game.input.keyboard.isDOWN(Phaser.Keyboard.X)) {
          sprite.body.velocity.y = -350;
          sprite.animations.play('jump',5,true);
     }
